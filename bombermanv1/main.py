76e67189ed494cac0d7a977fb805ec42c06de41e
monroe-hub <tuckerj25@asmsa.org>
2025-03-11 11:32:12 -0500

    Added Player Movement
    Player movement has been added with boundary detection and wall detection implemented as well. Additionally, the map system was consolidated into a class, greatly cleaning up the code.

--- bombermanv1/main.py
+++ bombermanv1/main.py
@@ -8,30 +8,49 @@ class Player:
     def __init__(self, grid_pos):
         
         self.position = grid_pos
-        self.direction = None
+        self.direction = None 
 
         self.health = 3
 
+    def try_move(self, active_map):
 
-    def conjure(coords):
+        if self.direction == 'up':
+            if active_map[self.position[0] - 1, self.position[1]] >= 0 and active_map[self.position[0] - 1, self.position[1]] <= active_map.shape[0]: 
+                                                                                      
+                if active_map[self.position[0] - 1, self.position[1]] == 0:
+                    self.move()
 
-        p5.push()
-        p5.no_stroke()
-        p5.fill(0, 135, 135)
-        p5.circle(coords[0], coords[1], 64)
-        p5.pop()
+        if self.direction == 'down':
+            if active_map[self.position[0] + 1, self.position[1]] >= 0 and active_map[self.position[0] - 1, self.position[1]] <= active_map.shape[0]:
+                if active_map[self.position[0] + 1, self.position[1]] == 0:
+                    self.move()
+            
+        if self.direction == 'right':
+            if active_map[self.position[0], self.position[1] + 1] >= 0 and active_map[self.position[0] - 1, self.position[1]] <= active_map.shape[0]:
+                if active_map[self.position[0], self.position[1] + 1] == 0:
+                    self.move()
+            
+        if self.direction == 'left':
+            if active_map[self.position[0], self.position[1] - 1] >= 0 and active_map[self.position[0] - 1, self.position[1]] <= active_map.shape[0]:
+                if active_map[self.position[0], self.position[1] - 1] == 0:
+                    self.move()
+        
 
     def move(self):
 
-        if self.direction == None: pass
-        elif self.direction == 'up': 
-            self.position = (self.position[0], self.position[1] + 1)
-        elif self.direction == 'down': 
-            self.position = (self.position[0], self.position[1] - 1)
-        elif self.direction == 'right': 
-            self.position = (self.position[0] + 1, self.position[1])
-        elif self.direction == 'left': 
-            self.position = (self.position[0] - 1, self.position[1])
+            #This seems inverted, and that's because it is: numpy arrays have the origin in the top left
+            
+            if self.direction == 'up': 
+                self.position = (self.position[0] - 1, self.position[1])
+            elif self.direction == 'down': 
+                self.position = (self.position[0] + 1, self.position[1])
+            elif self.direction == 'right': 
+                self.position = (self.position[0], self.position[1] + 1)
+            elif self.direction == 'left': 
+                self.position = (self.position[0], self.position[1] - 1)
+                
+            self.direction = None
+        
 
     def throw_bomb():
         pass
@@ -46,6 +65,73 @@ def load_image(file_path):
         print("Error loading image")
         return None
 
+class Map():
+
+    def __init__(self, array, tile_size=64):
+        
+        self.init_coords = [100, 100]
+        self.array = array
+        self.tile_size = tile_size
+
+        self.coordinate_map = np.empty(shape=self.array.shape, dtype=object)
+        self.active_map = np.copy(self.array)
+
+        for row_index in range(0, array.shape[0]):
+            for col_index in range(0, array.shape[1]):
+
+                x = self.init_coords[0] + col_index * tile_size
+                y = self.init_coords[1] + row_index * tile_size
+
+                self.coordinate_map[row_index, col_index] = (x, y)
+
+    def render_map(self):
+        """This will draw a grid for the corresponding map array."""
+        
+
+        for row_index, row_values in enumerate(self.active_map):
+            for col_index, position in enumerate(row_values):
+                #I know that every tile_size should be a self but I'm lazy
+                tile_size = self.tile_size
+
+                x = self.init_coords[0] + col_index * tile_size
+                y = self.init_coords[1] + row_index * tile_size
+
+                if position == 1:
+                    p5.image(steel_brick, x, y, tile_size, tile_size)
+
+                elif position == 2:
+                    
+                    p5.image(break_brick, x, y, tile_size, tile_size)
+
+                elif position == 3:
+                    p5.push()
+                    p5.fill(0)
+                    p5.square(x, y, tile_size)
+                    p5.pop()
+
+                elif position == 4:
+
+                    p5.push()
+                    p5.no_stroke()
+                    p5.fill(0, 135, 135)
+                    p5.circle(x, y, tile_size)
+                    p5.pop()
+
+    def refresh_map(self, player):
+        """Recalculates map logic, updating it within the object. Requires a player object as input for various calculations."""
+
+
+        for row_index in range(0, self.active_map.shape[0]):
+            for col_index in range(0, self.active_map.shape[1]):
+
+                x = row_index
+                y = col_index
+
+                if (x, y) == player.position: self.active_map[x, y] = 4
+                elif self.active_map[x, y] == 4: self.active_map[x, y] = 0
+
+
+
 def load_assets():
     """Attempts to load all relevant assets (images, etc.) and places them into the global namespace."""
     global break_brick, steel_brick
@@ -55,65 +141,6 @@ def load_assets():
     # print(image_folder)
     break_brick = load_image(image_folder + path.sep + "destroyable_brick.png")
     steel_brick = load_image(image_folder + path.sep + "steel_brick.png")
-
-
-
-def render_map(array, tile_size = 80):
-    """This will draw a grid for the corresponding map array."""
-
-    init_coords = [100, 100]
-    
-
-    for row_index, row_values in enumerate(array):
-        for col_index, position in enumerate(row_values):
-            
-            x = init_coords[0] + col_index * tile_size
-            y = init_coords[1] + row_index * tile_size
-
-            if position == 2:
-                
-                p5.image(break_brick, x, y, tile_size, tile_size)
-            elif position == 1:
-                p5.image(steel_brick, x, y, tile_size, tile_size)
-
-            elif position == 3:
-                p5.push()
-                p5.fill(0)
-                p5.square(x, y, tile_size)
-                p5.pop()
-
-            elif position == 4:
-
-                p5.push()
-                p5.no_stroke()
-                p5.fill(0, 135, 135)
-                p5.circle(x, y, tile_size)
-                p5.pop()
-
-def create_coordinate_space(array, tile_size = 80):
-    """Creates from a given array two returned arrays: a coordinate map, where each cartesian grid coordinate in the array corresponds to its drawn position in 
-        Processing's screen-space, and an active map, where each cartesian grid coordinate in the array 
-        corresponds to a possible data value; this begins with the values of the parameterized array.
-    """
-
-
-    init_coords = [100, 100]
-
-    coordinate_map = np.empty(shape=array.shape, dtype=object)
-    active_map = np.copy(array)
-
-    for row_index in range(0, array.shape[0]):
-        for col_index in range(0, array.shape[1]):
-
-            x = init_coords[0] + col_index * tile_size
-            y = init_coords[1] + row_index * tile_size
-
-            coordinate_map[row_index, col_index] = (x, y)
-
-
-    return active_map, coordinate_map
-
-
 
 def settings():
     p5.size(1000, 1000)
@@ -126,34 +153,45 @@ def setup():
     load_assets()
     p5.background(255)
 
-    global test_map
+    
     test_map = np.random.randint(0, 3, size=(11, 13))
-    
-    global coordinate_map, active_map
-    coordinate_map = create_coordinate_space(test_map, tile_size=64)[1]
-    active_map = create_coordinate_space(test_map, tile_size=64)[0]
-    
+
+    global maps
+    maps = Map(test_map)
+
     global bomberboy
     bomberboy = Player((1, 1))
-    active_map[bomberboy.position[0], bomberboy.position[1]] = 4
+    # active_map[bomberboy.position[0], bomberboy.position[1]] = 4
 
 def draw():
-    global active_map
-    #recalculates active map, looking for changes
-    active_map = create_coordinate_space(active_map, tile_size=64)[0]
+    p5.background(255)
+    
+
+    #Checks to see if a move is queued
+    bomberboy.try_move(active_map=maps.active_map)
+
+    maps.refresh_map(bomberboy)
+    maps.render_map()
+
+def key_pressed():
+
+    key = str(p5.key).lower()
+
+    # print(key)
+
+    if key == 'w': 
+        bomberboy.direction = 'up'
+    elif key == 's': 
+        bomberboy.direction = 'down'
+    elif key == 'd': 
+        bomberboy.direction = 'right'
+    elif key == 'a': 
+        bomberboy.direction = 'left'
+
+    #Debug statement, keep in mind that the coordinate here will not be updated yet
+    # print(f"Keypress: {key} Direction: {bomberboy.direction} Coordinate: {bomberboy.position}")
 
 
-    global test_map
-    render_map(active_map, tile_size=64)
-
-def key_pressed(key):
-
-    key = str(key).lower()
-
-    if key == 'w': bomberboy.direction = 'up'
-    elif key == 's': bomberboy.direction = 'down'
-    elif key == 'd': bomberboy.direction = 'right'
-    elif key == 'a': bomberboy.direction = 'left'
         
 
     

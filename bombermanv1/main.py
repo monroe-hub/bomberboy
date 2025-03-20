a7db0b4f926e78bc0758edadf6876784fc5ad71e
monroe-hub <tuckerj25@asmsa.org>
2025-03-20 11:30:56 -0500

    Bombs!
    Bombs are now implemented, can be placed, explode only the correct tiles and do damage to the player. Some movement logic was also changed

--- bombermanv1/main.py
+++ bombermanv1/main.py
@@ -15,7 +15,7 @@ class Player:
         self.visual_direction = 'down'
 
         self.target_coord = None
-        self.check_space()
+        # self.check_space()
 
 
         self.health = 3
@@ -64,25 +64,18 @@ class Player:
     def move(self):
         # if self.direction != None: self.visual_direction = self.direction
 
-        # self.check_space()
-
-        # if self.visual_direction == self.direction and self.target_coord is not None:
-
-        #     self.position = self.target_coord
-        # else: 
-        #     self.visual_direction = self.visual_direction
-
-        # self.check_space()
-        # self.direction = None
-
         self.check_space()
 
-        # Allow movement only if the direction is valid and the target is not None
-        if self.direction is not None and self.target_coord is not None:
-            self.position = self.target_coord
-            self.visual_direction = self.direction  # Update visual direction only after movement
+        if self.visual_direction == self.direction:
 
-        self.direction = None  # Reset movement after processing
+            self.position = self.target_coord
+            self.check_space()
+
+        else: 
+            self.visual_direction = self.direction
+
+        
+        self.direction = None
         
 
     def place_bomb(self, bomb_list):
@@ -94,16 +87,22 @@ class Player:
 
 class Bomb:
 
+    #TODO here: create explosion trails and pulsing bombs
+
     def __init__(self, grid_pos):
 
         self.grid_pos = grid_pos
-        self.time_left = 4.0
-
-    pass
-
+        self.bomb_strength = 1
         
-    
+        self.spawn_time = p5.millis()  
+        self.time_left = 4000  
 
+    def explosion_check(self):
+
+        if p5.millis() - self.spawn_time >= self.time_left:
+            return True
+        else:
+            return False
 
 class Map:
 
@@ -165,13 +164,26 @@ class Map:
                         p5.pop_matrix()
 
                 elif position == 5:
-
-                    p5.push()
-                    p5.fill(0)
-                    p5.circle(x, y, self.tile_size)
-                    p5.pop()
-
                     
+                    # for bomb in bomb_list:
+                        # if bomb.grid_pos == (x, y):
+
+                            # progress = p5.constrain((p5.millis() - bomb.spawn_time) / bomb.time_left, 0, 1)
+
+                            
+                            p5.push()
+
+                            #TODO: make the color shift start working
+                            # p5.fill(255 * progress, 0, 0)
+                            
+                            p5.fill(0)
+                            p5.circle(x, y, self.tile_size)
+                            p5.pop()
+
+    def draw_border(self):
+
+        #TODO: this will draw a layer of uninteractable steel bricks (1) around the map
+        pass
 
     def refresh_map(self, player, bomb_list):
         """Recalculates map logic, updating it within the object. Requires a player object as input for various calculations."""
@@ -186,9 +198,58 @@ class Map:
                 if (x, y) == player.position: self.active_map[x, y] = 4
                 elif self.active_map[x, y] == 4: self.active_map[x, y] = 0
 
-                for bomb in bomb_list:
+                for i, bomb in enumerate(bomb_list):
                     if bomb.grid_pos == (x, y):
                         self.active_map[x, y] = 5
+
+                        if bomb.explosion_check():
+                            for i in range(0, bomb.bomb_strength + 1):
+
+                                #This looks digusting but essentially all it does is loop through the neighboring directions, and blow em up
+                                p5.push()
+                                p5.fill(230, 44, 7)
+
+                                if self.active_map[x + i, y] != 1 and (0 <= x + i <= self.active_map.shape[0] - 1):
+
+                                    if self.active_map[x + i, y] == 4: player.health -= 1
+
+                                    self.active_map[x + i, y] = 0
+                                    p5.circle(self.coordinate_map[x + i, y][0], self.coordinate_map[x + i, y][1], self.tile_size)
+
+                                
+
+                                if self.active_map[x - i, y] != 1 and (0 <= x - i <= self.active_map.shape[0] - 1):
+
+                                    if self.active_map[x - i, y] == 4: player.health -= 1
+
+                                    self.active_map[x - i, y] = 0
+                                    p5.circle(self.coordinate_map[x - i, y][0], self.coordinate_map[x - i, y][1], self.tile_size)
+
+                                
+
+                                if self.active_map[x, y + i] != 1 and (0 <= y + i <= self.active_map.shape[1] - 1):
+                                    
+                                    if self.active_map[x, y + i] == 4: player.health -= 1
+                                    
+                                    self.active_map[x, y + i] = 0
+                                    p5.circle(self.coordinate_map[x , y + i][0], self.coordinate_map[x, y + i][0], self.tile_size)
+
+                                
+
+                                if self.active_map[x, y - i] != 1 and (0 <= y - i <= self.active_map.shape[1] - 1):
+
+                                    if self.active_map[x, y - i] == 4: player.health -= 1
+
+                                    self.active_map[x, y - i] = 0
+                                    p5.circle(self.coordinate_map[x, y - i][0], self.coordinate_map[x, y - i][0], self.tile_size)
+
+                                if self.active_map[x, y] != 1:
+                                    self.active_map[x, y] = 0
+                                    p5.circle(self.coordinate_map[x, y][0], self.coordinate_map[x, y][1], self.tile_size)
+
+                                p5.pop()
+
+                            bomb_list.pop(i-1)
 
 def generate_base_map(size=(10, 10)):
 
@@ -203,8 +264,8 @@ def generate_base_map(size=(10, 10)):
                 if x % 2 != 0 and y % 2 != 0:
                     array[x, y] = 1
 
-                # elif np.random.randint(0, 1) % 1 == 0:
-                    # array[x, y] = 2
+                elif np.random.randint(0, 6) % 2 == 0:
+                    array[x, y] = 2
 
     return array
 
@@ -257,11 +318,15 @@ def setup():
     global bomb_list
     bomb_list = []
 
+
 def draw():
     p5.background(255)
     
     maps.refresh_map(bomberboy, bomb_list=bomb_list)
     maps.render_map(bomberboy)
+
+    
+    
   
 
 def key_pressed():
@@ -272,32 +337,28 @@ def key_pressed():
 
         bomberboy.direction = 'up'
         #Checks if the move entering a valid space
+        bomberboy.move()
 
     elif key == 's': 
 
         bomberboy.direction = 'down'
+        bomberboy.move()
 
     elif key == 'd': 
 
         bomberboy.direction = 'right'
+        bomberboy.move()
 
     elif key == 'a': 
 
         bomberboy.direction = 'left'
+        bomberboy.move()
 
     elif key == ' ':
         
         bomberboy.place_bomb(bomb_list=bomb_list)
         return
     
-    #Default action is movement if not bomb
-
-    bomberboy.move()
-    
-
-
-
-
     #Debug statement, keep in mind that the coordinate here will not be updated yet
     # print(f"Keypress: |{key}| Direction: {bomberboy.direction} Coordinate: {bomberboy.position}")
     # print(bomberboy.target_coord, len(bomb_list))
